4fd5c7d96584ac26b82f2e29e907db190bf87832
Pedro.Lopes <pedrolopes.dev@gmail.com>
2019-01-07 10:16:28 -0200

    Added gitignore

---
+++ MovieList/MovieList/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  MovieList
+//
+//  Created by Joao Pedro Lopes Felix da Silva on 07/01/19.
+//  Copyright © 2019 Joao Pedro Lopes Felix da Silva. All rights reserved.
+//
+
+import Foundation
---
+++ MovieList/MovieList/Search Screen/SearchScreenViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SearchScreenViewController.swift
+//  MovieList
+//
+//  Created by Joao Pedro Lopes Felix da Silva on 04/01/19.
+//  Copyright © 2019 Joao Pedro Lopes Felix da Silva. All rights reserved.
+//
+
+import Foundation
